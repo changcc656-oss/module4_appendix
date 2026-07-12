dfe8c3a2553865e7ecd7e7a1ecd7667f95fb0f29
changcc656-oss <changcc656@gmail.com>
2026-07-12 16:52:36 +0800

    Delete 3_gls directory

--- 3_gls/open_dve.sh
+++
@@ -1,1 +0,0 @@
-dve -vpd multiplier_mc_gate.vpd -session session.multiplier_mc_gate.vpd.tcl&
--- 3_gls/run_vcs.sh
+++
@@ -1,8 +0,0 @@
-vcs -full64 -sverilog -timescale=1ns/1ps -debug_access+all \
-    /pdk/SAED32_EDK/lib/stdcell_rvt/verilog/saed32nm.v \
-    mul32_mc_netlist.v \
-    tb_multiplier_mc_gate.v \
-    -o simv_mc_gate \
-    -l compile_mc_gate.log
-
-./simv_mc_gate +sdf-l sim_mc_gate.log
